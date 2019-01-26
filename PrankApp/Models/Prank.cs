706784582b9cb894e212c879c181f6ddc2dba10e
Timothy J Freese <freese.tim@gmail.com>
2019-01-26 10:38:36 -0600

    models

---
+++ PrankApp/Models/Prank.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrankApp.Models
+{
+    public class Prank
+    {
+        public string Name { get; set; }
+
+        public string Id { get; set; }
+
+
+        public List<Device> DeviceList { get; set; }
+
+
+        public int TurnIndex { get; set; }
+
+    }
+}
